0c682d8acf7606b9995ce6cd90a27c9221c8e5ca
wsrmob-04 <sema-devyatov@mail.ru>
2022-05-05 16:50:28 +0500

    start commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity3.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity3 : AppCompatActivity() {
+    lateinit var email: EditText
+    lateinit var password: EditText
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+    }
+    fun signin(view: View){
+        if (email.text.toString().isNotEmpty() && password.text.toString().isNotEmpty()){
+
+        }
+        else{
+            val alert = AlertDialog.Builder(this)
+                .setTitle("Ошибка")
+                .setMessage("У вас есть не заполненные поля")
+                .setPositiveButton("Ok",null)
+                .create()
+                .show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MenuActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myapplication.databinding.ActivityMain3Binding
+
+class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain3Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main4)
+        //navView.setupWithNavController(navController)
+    }
+}
